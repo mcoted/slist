f48ffc4b2102aafa02e3eeeb167bbaeeb7bc7a4b
Martin Cote <cote.martin@gmail.com>
2015-11-23 17:17:51 -0500

    Working for-macro.

--- test_macros.lisp
+++ test_macros.lisp
@@ -1,43 +1,11 @@
-;(defmacro (for var in values body)	
-;
-;(for a in '(1 2 3) (println a))
+(defmacro (for var in values body)
+	'(letrec ((loop (lambda (x)
+						(if (empty? x)
+							'()
+							(let ((,var (car x)))
+								(begin
+									(eval ,body)
+									(loop (cdr x))))))))
+		(loop ,values)))
 
-;(define (f v)
-;	(let ((g (lambda (x) (if (= x 0) 0 (g (- x 1))))))
-;		(g v)))
-;(f 1)
-
-;(define (f v)
-;	((lambda (g) (begin (set! g g-value) (g v)))
-;		(lambda (x) (if (= x 0) 0 (g (- x 1))))))
-;(f 1)
-
-; (define (f)
-; 	((lambda (x) (x))
-; 		(lambda () (x))))
-;
-;(define (f)
-;	(let ((x (lambda () (x))))
-;		(x)))
-;
-;;;;;
-;
-;(define (f)
-;	((lambda (x) (x)) 
-;		((lambda () (x)))))
-;
-;;; -->
-;
-;(define (f)
-;	((lambda (x) (set! x (some-lambda (x))) (x))
-;		0))
-;
-;;;;;
-;
-;(define (f)
-;	((lambda (x) '())
-;		1))
-;
-(define (f v)
-	(letrec ((x (lambda (c) (if (= c 1) 1 (+ 1 (f (- c 1)))))))
-		(x v)))
+(for a in '(1 2 3) (println a))
